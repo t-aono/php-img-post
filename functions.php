89123246bf912d7e9757712c3358c3c312ba4b03
Takahisa Aono <spain3405@gmail.com>
2020-01-22 10:41:14 +0900

    ClearDB接続

--- functions.php
+++ functions.php
@@ -1,13 +1,21 @@
 <?php
 // データベースに接続
 function connectDB() {
-    $param = 'mysql:host=mysql;dbname=image_post';
-    try {
-        $pdo = new PDO($param, 'root', 'pass');
-        return $pdo;
-
-    } catch (PDOException $e) {
-        echo $e->getMessage();
-        exit();
+  try {
+    if ($_SERVER['SERVER_NAME'] == 'localhost') {
+      // 開発環境のMySQLに接続
+      $param = 'mysql:host=mysql;dbname=image_post';
+      $pdo = new PDO($param, 'root', 'pass');
+      return $pdo;
+    
+    } else {
+      // herokuのClearDBに接続
+      $heroku_db = parse_url(getenv('CLEARDB_DATABASE_URL'));
+      $db = new PDO('mysql:dbname='.substr($heroku_db['path'], 1).';host='.$heroku_db['host'].';charset=utf8', $heroku_db['user'], $heroku_db['pass']);
     }
+
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    exit();
+  }
 }
